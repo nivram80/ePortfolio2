c2e68b4288452822a92b5dd3a94625d38a779b0e
Todd Nichols <nivram80@gmail.com>
2013-08-31 22:38:13 -0500

    added js fancybox plugin and pricing tables

--- public/js/eportfolio2.js
+++ public/js/eportfolio2.js
@@ -1,1 +1,9 @@
-$('a[href^="http://"]').attr('target','_blank');
+$(document).ready(function() {
+	$('.iframe').fancybox({
+		width : '80%',
+		height : '80%',
+		type : 'iframe',
+		scrolling : 'no'
+	}); // end fancybox
+	$('a[href^="http://"]').attr('target','_blank');
+}); // end ready
