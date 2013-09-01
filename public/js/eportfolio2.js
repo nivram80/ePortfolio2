84d33ae249626bcaa83f7f7650550a88063aacd0
Todd Nichols <nivram80@gmail.com>
2013-09-01 15:17:13 -0500

    removed deleted files

--- public/js/eportfolio2.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$('.iframe').fancybox({
-		width : '80%',
-		height : '80%',
-		type : 'iframe',
-		scrolling : 'no'
-	}); // end fancybox
-	$('a[href^="http://"]').attr('target','_blank');
-}); // end ready
