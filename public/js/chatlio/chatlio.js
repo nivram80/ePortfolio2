c7f15d4f5ac33f391dc99aef9b1eb9615ba7ccc3
Todd Nichols <nivram80@gmail.com>
2019-01-24 17:21:49 -0600

    auto message test

--- public/js/chatlio/chatlio.js
+++ public/js/chatlio/chatlio.js
@@ -5,9 +5,14 @@ document.addEventListener('chatlio.ready', function (e) {
     plan: 'speed',
     favoriteColor: 'royal blue'
   });
+
+  var message = 'Hello! 👋\n\nHere are your instructor Kim and the pelvina team.\n\nYou can reach us Mon-Fri: 9: 00-22: 00 here in the chat and by phone on 📞 089 215 544 970, also anytime and on weekends also by e-mail to support@pelvina.de.\n\n** Important: ** If you have acute health problems,, please contact a doctor immediately. Please note that as part of our course we do not offer medical advice or advice on the detection or treatment of disease.\n\nBut now to you ... 🤗\n\nHow can we help you?'
   _chatlio.configure({
-    "onlineTitle": "Question? Select here.",
-    "autoResponseMessage": "Question for moi? Just type it below and we are online and ready to answer.",
-    "onlineMessagePlaceholder": "Type here my friend..."
+    'onlineTitle': 'Question? Select here.',
+    'onlineMessagePlaceholder': 'Type here my friend...',
+    'autoResponseMessage': message
   });
+  if (_chatlio.isOnline()) {
+    document.getElementById('chat-menu')
+  }
 }, false);
