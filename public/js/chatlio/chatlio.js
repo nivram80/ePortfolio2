63c2be627c3a4a1f965a535667d7d58c7a3f97c3
Todd Nichols <nivram80@gmail.com>
2019-01-24 14:20:18 -0600

    comment out identify function for testing

--- public/js/chatlio/chatlio.js
+++ public/js/chatlio/chatlio.js
@@ -1,10 +1,10 @@
 document.addEventListener('chatlio.ready', function (e) {
-  _chatlio.identify('user1234', {
-    name: 'Willie Wilson',
-    email: 'ww@royals.com',
-    plan: 'speed',
-    favoriteColor: 'royal blue'
-  });
+  // _chatlio.identify('user1234', {
+  //   name: 'Willie Wilson',
+  //   email: 'ww@royals.com',
+  //   plan: 'speed',
+  //   favoriteColor: 'royal blue'
+  // });
   _chatlio.configure({
     "onlineTitle": "Question? Select here.",
     "autoResponseMessage": "Question for moi? Just type it below and we are online and ready to answer.",
