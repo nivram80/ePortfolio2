c371ee01a223f0a136a70f224c71bd19ca741cd5
Todd Nichols <nivram80@gmail.com>
2019-01-30 14:29:35 -0600

    add back viewport meta tag

--- public/js/chatlio/embed-code.js
+++ public/js/chatlio/embed-code.js
@@ -1,15 +1,15 @@
 // omahaareaman
-// window._chatlio = window._chatlio||[];
-// !function(){ var t=document.getElementById("chatlio-widget-embed");if(t&&window.ChatlioReact&&_chatlio.init)return void _chatlio.init(t,ChatlioReact);for(var e=function(t){return function(){_chatlio.push([t].concat(arguments)) }},i=["configure","identify","track","show","hide","isShown","isOnline", "page", "open", "showOrHide"],a=0;a<i.length;a++)_chatlio[i[a]]||(_chatlio[i[a]]=e(i[a]));var n=document.createElement("script"),c=document.getElementsByTagName("script")[0];n.id="chatlio-widget-embed",n.src="https://w.chatlio.com/w.chatlio-widget.js",n.async=!0,n.setAttribute("data-embed-version","2.3");
-//    n.setAttribute('data-widget-id','1e23b046-525a-4ac1-519d-c06e0da2315e');
-//    c.parentNode.insertBefore(n,c);
-// }();
+window._chatlio = window._chatlio||[];
+!function(){ var t=document.getElementById("chatlio-widget-embed");if(t&&window.ChatlioReact&&_chatlio.init)return void _chatlio.init(t,ChatlioReact);for(var e=function(t){return function(){_chatlio.push([t].concat(arguments)) }},i=["configure","identify","track","show","hide","isShown","isOnline", "page", "open", "showOrHide"],a=0;a<i.length;a++)_chatlio[i[a]]||(_chatlio[i[a]]=e(i[a]));var n=document.createElement("script"),c=document.getElementsByTagName("script")[0];n.id="chatlio-widget-embed",n.src="https://w.chatlio.com/w.chatlio-widget.js",n.async=!0,n.setAttribute("data-embed-version","2.3");
+   n.setAttribute('data-widget-id','1e23b046-525a-4ac1-519d-c06e0da2315e');
+   c.parentNode.insertBefore(n,c);
+}();
 
 
 // test-omahaareaman
-window._chatlio = window._chatlio||[];
-!function(){ var t=document.getElementById("chatlio-widget-embed");if(t&&window.ChatlioReact&&_chatlio.init)return void _chatlio.init(t,ChatlioReact);for(var e=function(t){return function(){_chatlio.push([t].concat(arguments)) }},i=["configure","identify","track","show","hide","isShown","isOnline", "page", "open", "showOrHide"],a=0;a<i.length;a++)_chatlio[i[a]]||(_chatlio[i[a]]=e(i[a]));var n=document.createElement("script"),c=document.getElementsByTagName("script")[0];n.id="chatlio-widget-embed",n.src="https://w.chatlio.com/w.chatlio-widget.js",n.async=!0,n.setAttribute("data-embed-version","2.3");
-   n.setAttribute('data-widget-id','2eb5b3dd-71a4-468c-4475-07adf2563812');
-   c.parentNode.insertBefore(n,c);
-}();
+// window._chatlio = window._chatlio||[];
+// !function(){ var t=document.getElementById("chatlio-widget-embed");if(t&&window.ChatlioReact&&_chatlio.init)return void _chatlio.init(t,ChatlioReact);for(var e=function(t){return function(){_chatlio.push([t].concat(arguments)) }},i=["configure","identify","track","show","hide","isShown","isOnline", "page", "open", "showOrHide"],a=0;a<i.length;a++)_chatlio[i[a]]||(_chatlio[i[a]]=e(i[a]));var n=document.createElement("script"),c=document.getElementsByTagName("script")[0];n.id="chatlio-widget-embed",n.src="https://w.chatlio.com/w.chatlio-widget.js",n.async=!0,n.setAttribute("data-embed-version","2.3");
+//    n.setAttribute('data-widget-id','2eb5b3dd-71a4-468c-4475-07adf2563812');
+//    c.parentNode.insertBefore(n,c);
+// }();
 
